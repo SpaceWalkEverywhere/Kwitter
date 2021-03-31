3c0cd5ced8079519f2000bb8546d63f3850d640d
SpaceWalkEverywhere <74199628+SpaceWalkEverywhere@users.noreply.github.com>
2021-03-31 18:02:43 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,14 +1,14 @@
 var firebaseConfig = {
-apiKey: "AIzaSyCVWrQYrHjze_2rVDNuARf8eecp115Uq9U",
-authDomain: "chatweb-53865.firebaseapp.com",
-databaseURL: "https://chatweb-53865-default-rtdb.firebaseio.com",
-projectId: "chatweb-53865",
-storageBucket: "chatweb-53865.appspot.com",
-messagingSenderId: "889379280424",
-appId: "1:889379280424:web:1275d6886e7a5e4864071d"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+      apiKey: "AIzaSyCVWrQYrHjze_2rVDNuARf8eecp115Uq9U",
+      authDomain: "chatweb-53865.firebaseapp.com",
+      databaseURL: "https://chatweb-53865-default-rtdb.firebaseio.com",
+      projectId: "chatweb-53865",
+      storageBucket: "chatweb-53865.appspot.com",
+      messagingSenderId: "889379280424",
+      appId: "1:889379280424:web:1275d6886e7a5e4864071d"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
 username=localStorage.getItem("username");
 document.getElementById("username").innerHTML="welcome "+username+" !!!";
 function addroomname(){
