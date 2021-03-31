4c3c6d4d7595b8021334374d461f4c4862d902bc
SpaceWalkEverywhere <74199628+SpaceWalkEverywhere@users.noreply.github.com>
2021-03-31 17:56:16 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -8,10 +8,25 @@ appId: "1:889379280424:web:1275d6886e7a5e4864071d"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-
-
+username=localStorage.getItem("username");
+document.getElementById("username").innerHTML="welcome "+username+" !!!";
+function addroomname(){
+      roomname=document.getElementById("rn").value;
+      firebase.database().ref("/").child(roomname).update({
+            purpose:"addrn",
+      });
+      localStorage.setItem("rn",roomname);
+      window.location="kwitterpage.html";
+}
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
        Room_names = childKey;
-      
+      console.log("room name-"+Room_names);
+      row="<div class='room_name' id="+Room_names+" onclick='redtorm(this.id);'>#"+Room_names+"</div><hr>";
+      document.getElementById("output").innerHTML+=row;
       });});}
 getData();
+function redtorm(name){
+      console.log(name);
+      localStorage.setItem("rn",name);
+      window.location="kwitterpage.html";
+}
